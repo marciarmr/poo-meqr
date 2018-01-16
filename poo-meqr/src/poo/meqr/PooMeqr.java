bc34dd92c102db35fbe19345f4b25ae49803290b
T-102 <T-102@PC256>
2018-01-15 19:38:06 -0600

    Mi primer clase de poo2 Marcia Quesada

---
+++ poo-meqr/src/poo/meqr/PooMeqr.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poo.meqr;
+
+/**
+ *
+ * @author T-102
+ */
+public class PooMeqr {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
